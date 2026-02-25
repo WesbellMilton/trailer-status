bb9a1c3cf66e431bf2c2a1fa1499534667bead07
WesbellMilton <cdhanraj@wesbelllogistics.com>
2026-02-25 11:53:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const WebSocket = require("ws");
 
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });

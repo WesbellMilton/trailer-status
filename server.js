f5fd5411658a6adbd0be74c00fe54b419e291b4a
WesbellMilton <cdhanraj@wesbelllogistics.com>
2026-02-25 12:31:39 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-
+{
+  "name": "dock-board",
+  "version": "1.0.0",
+  "main": "server.js",
+  "type": "commonjs",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.19.2",
+    "ws": "^8.17.0"
+  }
+}
